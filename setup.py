5d1a77f18b4a39e380304faa19ea3ea25b02d124
Rafael Batista <raphab33@hotmail.com>
2025-11-12 03:04:36 -0300

    fix: update version to 1.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hermes-notifier",
-    version="1.1.0",
+    version="1.2.0",
     author="Rafael Batista",
     author_email="raphab33@hotmail.com",
     description="Python/Django plugin for Hermes notifications system with SSE and push notification support",
